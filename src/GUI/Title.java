cb01c3a582748c2c5f17e625b5b1a361156d3ae7
Cherr0 <adsl1664@gmail.com>
2020-11-16 22:13:50 +0900

    Title 삭제

--- src/GUI/Title.java
+++ src/GUI/Title.java
@@ -10,9 +10,6 @@ import javax.swing.JTextField;
 
 public class Title extends JPanel {
 	private JTextField textField;
-//	public static ArrayList<JLabel> rainText = new ArrayList<>();
-//	public static JLabel[] life = new JLabel[3];
-
 	public static ImageIcon t_background, upIcon, downIcon;
 	
 	
@@ -20,17 +17,5 @@ public class Title extends JPanel {
 	public Title() {
 		setLayout(null);
 		
-		
-		t_background = new ImageIcon("img/background-image.png");
-		textField = new JTextField();
-		textField.setBounds(290, 171, 53, 21);
-		add(textField);
-		textField.setColumns(10);
-		
 	}
-	
-//	public void paintComponent(Graphics g) {
-//		super.paintComponent(g);
-//		g.drawImage(t_background.getImage(), 0, 0, null); // 0,0 좌표부터 이미지를 뿌림
-//	}
 }

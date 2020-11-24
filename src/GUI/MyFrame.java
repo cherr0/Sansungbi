51912f8cee332d2024db5836875fd5fff3274e27
Cherr0 <adsl1664@gmail.com>
2020-11-24 22:08:02 +0900

    MyFrame: 리스너 연결 변경

--- src/GUI/MyFrame.java
+++ src/GUI/MyFrame.java
@@ -10,27 +10,28 @@ public class MyFrame extends JFrame {
 
 	MyListener ml;
 
+	public Client client;
 	public NPanel nPanel;
 	public EPanel ePanel;
 	public SPanel sPanel;
 	public ClientPanel cPanel;
 
-    public MyFrame(String name){
+    public MyFrame(Client client,String name){
     	setTitle(name);
 		Rectangle range = new Rectangle(0, 0, 700, 600);
 		setBounds(range);		// 절대값으로 위치 설정
 		setResizable(false);	// 창 크기 조절 불가
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
+
+		this.client = client;
 		ml = new MyListener(this);
 
-		Client client = new Client();
 
-
-		nPanel = new NPanel();
+		nPanel = new NPanel(ml);
 		cPanel = new ClientPanel(client);
-		ePanel = new EPanel();
-		sPanel = new SPanel();
+		ePanel = new EPanel(ml);
+		sPanel = new SPanel(ml);
 
 		add(nPanel, BorderLayout.NORTH);
 		add(cPanel, BorderLayout.CENTER);
@@ -39,4 +40,8 @@ public class MyFrame extends JFrame {
 
 		setVisible(true);
     }
+
+    public void setMyListener(MyListener ml){
+    	this.ml = ml;
+	}
 }

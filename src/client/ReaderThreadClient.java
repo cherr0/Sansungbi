09659299951fe1daf7d71b452613f05406fc8429
Cherr0 <adsl1664@gmail.com>
2020-11-26 22:14:57 +0900

    ReaderThread: 메세지 case 추가

--- src/client/ReaderThreadClient.java
+++ src/client/ReaderThreadClient.java
@@ -6,8 +6,12 @@ import VO.Message;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ReaderThreadClient extends Thread {
+    // 클라이언트에서 돌아가는 스레드
+    // 서버에서 보낸 메세지를 읽어 행동 수행
+
     private Client client;
     private ObjectInputStream ois;
 
@@ -41,14 +45,30 @@ public class ReaderThreadClient extends Thread {
                 msg = (Message) ois.readObject();
 
                 msgType = msg.getType();
-
+                // 5 유저리스트 refresh, 6: userScore refresh
                 switch (msgType){
                     case 101:
                         dwList = selectWords(msg);
-
-                }
-
-            }
+                        client.sendPanelDrawWordList(dwList);
+                        break;
+                    case 11:    // 유저 리스트 갱신
+                        String[] nameList = msg.getuListString().split(",");
+                        System.out.println("유저 리스트: " + Arrays.toString(nameList)); // 콘솔에 유저 리스트 출력
+                        client.showUserList(nameList);
+                        break;
+                    case 12:    // 서버에서 DrawWordList 받아온다
+                        dwList = msg.getDWList();
+                        client.sendPanelDrawWordList(dwList);
+                        // server에서 panelstate를 받아온 다음 클라를 통해 넣는다
+                        panelState = msg.getPanelState();
+                        client.setPanelState(panelState);
+                        break;
+                    case 14:
+                        inputEntry = msg.getEntryString();
+                        client.matchWord(inputEntry);
+                        break;
+                } //switch end
+            }// while end
         }catch(IOException e){
             System.out.println("readerThread err: " + e);
         }catch(ClassNotFoundException e){
